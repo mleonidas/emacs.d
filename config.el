6d00dfcfa9e876ee0fd58507de6daa50c456f646
Mike Leone <mleone@attentivemobile.com>
2021-03-21 09:53:40 -0400

    wip
    you know it's emacs, constantly messing with configs

--- config.el
+++ config.el
@@ -94,6 +94,14 @@
       projectile-rails-spring-command "bin/spring"
       projectile-rails-zeus-command "bin/zeus")
 
+;; Golang
+
+(after! go-mode
+  (set-lookup-handlers! 'go-mode
+    :definition #'godef-jump
+    :references #'go-guru-referrers
+    :documentation #'godoc-at-point))
+
 (setq gofmt-command "goimports")
 (add-hook 'before-save-hook #'lsp-format-buffer)
 (add-hook 'before-save-hook #'lsp-organize-imports)
--- init.el
+++ init.el
@@ -57,7 +57,7 @@
        (evil +everywhere); come to the dark side, we have cookies
        file-templates    ; auto-snippets for empty files
        fold              ; (nigh) universal code folding
-       ;; (format +onsave)  ; automated prettiness
+       (format +onsave)  ; automated prettiness
        ;;god               ; run Emacs commands without modifier keys
        ;;lispy             ; vim for lisp, for people who don't like vim
        multiple-cursors  ; editing in many places at once
@@ -128,7 +128,7 @@
        ;;(haskell +dante)  ; a language that's lazier than I am
        ;;hy                ; readability of scheme w/ speed of python
        ;;idris             ;
-       ;;(java +meghanada) ; the poster child for carpal tunnel syndrome
+       (java +meghanada) ; the poster child for carpal tunnel syndrome javascript        ; all(hope(abandon(ye(who(enter(here))))))
        javascript        ; all(hope(abandon(ye(who(enter(here))))))
        ;;julia             ; a better, faster MATLAB
        ;;kotlin            ; a better, slicker Java(Script)
@@ -165,7 +165,7 @@
        ;;solidity          ; do you need a blockchain? No.
        ;;swift             ; who asked for emoji variables?
        ;;terra             ; Earth and Moon in alignment for performance.
-       ;;web               ; the tubes
+       ;; web               ; the tubes
 
        :email
        ;;(mu4e +gmail)
